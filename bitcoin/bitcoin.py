78e7725b626e26d890d5c00560af5952de8fa34b
m. mouse <akmphuljhb024@student.wethinkcode.co.za>
2025-03-03 01:41:56 +0200

    bitcoin api

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,47 @@
+# Bitcoin app
+# To run program: python {file name} {amount of bitcoin}
+
+"""
+Program that uses bitcoin api to give back realtime value of bit coin.
+"""
+
+import sys
+import json
+import requests
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Missing command-line argument")
+        sys.exit(1)
+
+    if sys.argv[1].isalpha():
+        print("Command-line argument is not a number")
+        sys.exit(1)
+
+    try:
+        response = requests.get(
+            "https://api.coincap.io/v2/assets/bitcoin" 
+        )
+        response.raise_for_status()
+    except requests.RequestException:
+        pass
+
+    crypto = response.json()
+    # print(crypto)
+
+    result = crypto['data']
+
+    price_per_bitcoin = result['priceUsd']
+    price_per_bitcoin = float(price_per_bitcoin)
+
+    quantity = float(sys.argv[1])
+
+    amount = quantity*price_per_bitcoin
+    amount = float(amount)
+
+    print(f"The current price of bitcoin is: ${price_per_bitcoin}")
+    print(f"> {quantity} bitcoin,are worth: ${amount:,.4f}")
+
+if __name__ == "__main__":
+    main()
